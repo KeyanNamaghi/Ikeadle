233f0eea5a4a1b645c58f6d7161195c5b0c72740
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2024-01-12 22:34:08 +0000

    🎉

--- vite.config.js
+++ vite.config.js
@@ -1,2 +1,8 @@
 import { defineConfig } from '@solidjs/start/config'
-export default defineConfig({})
+export default defineConfig({
+  start: {
+    server: {
+      preset: 'vercel',
+    },
+  },
+})
