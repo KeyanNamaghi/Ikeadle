6095be7abe614e6309456633db5da24031d661df
Keyan Namaghi <keyan.namaghi@sainsburys.co.uk>
2024-01-12 21:07:50 +0000

    😒

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,2 @@
 import { defineConfig } from '@solidjs/start/config'
-import vercel from 'solid-start-vercel'
-import solid from 'solid-start/vite'
-
-export default defineConfig({
-  plugins: [solid({ adapter: vercel({ edge: true }) })],
-})
+export default defineConfig({})
